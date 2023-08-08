d5ac0a449e12c8ad11e913223c0d6e5378b09cad
abrartariqulislam <islamtariqul010@gmail.com>
2023-08-08 18:30:20 +0600

    finished calculator

---
+++ app.js
@@ -0,0 +1,22 @@
+const input = document.querySelector("input");
+const buttons = document.querySelectorAll("button");
+
+let add = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.textContent === "=") {
+      add = eval(add);
+      input.value = add;
+    } else if (e.target.textContent === "DEL") {
+      add = add.substring(0, add.length - 1);
+      input.value = add;
+    } else if (e.target.textContent === "AC") {
+      add = "";
+      input.value = add;
+    } else {
+      add += e.target.textContent;
+      input.value = add;
+    }
+  });
+});
