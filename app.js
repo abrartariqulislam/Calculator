0f833fcb2602962958ccfdc860b513e2ad57bb23
abrartariqulislam <islamtariqul010@gmail.com>
2023-08-12 13:24:39 +0600

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const input = document.querySelector("input");
+const input = document.querySelector(".input b");
 const buttons = document.querySelectorAll("button");
 
 let add = "";
@@ -7,16 +7,21 @@ buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.textContent === "=") {
       add = eval(add);
-      input.value = add;
+      input.textContent = add;
     } else if (e.target.textContent === "DEL") {
       add = add.slice(0, add.length - 1);
-      input.value = add;
+      input.textContent = add;
     } else if (e.target.textContent === "AC") {
       add = "";
-      input.value = add;
-    }else {
+      input.textContent = add;
+    }
+    else if(e.target.textContent === "^"){
+      add += "**"
+      input.textContent = add
+    }
+    else {
       add += e.target.textContent;
-      input.value = add;
+      input.textContent = add;
     }
   });
 });
